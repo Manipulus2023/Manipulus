4106bfad79473cf20b51ad77de85c1deeec68a23
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2023-02-27 23:35:10 +0530

    added backend parts for vehicle master

---
+++ Server/src/main/java/com/Manipulus/arctic/vehicle/repository/VehicleRepository.java
@@ -0,0 +1,13 @@
+package com.Manipulus.arctic.vehicle.repository;
+
+
+import com.Manipulus.arctic.vehicle.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+    Optional<Vehicle> findVehicleById(Long id);
+
+    void deleteVehicleById(long id);
+}
