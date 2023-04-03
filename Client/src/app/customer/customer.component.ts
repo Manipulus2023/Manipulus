bc154d67b62ec5da2dc08ae22887891e0bd820b9
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2023-04-03 16:18:30 +0530

    working

--- Client/src/app/customer/customer.component.ts
+++ Client/src/app/customer/customer.component.ts
@@ -9,140 +9,155 @@ import { Subject } from 'rxjs';
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
-
-
-  public customers : Customer[] =[];
+  // Define class properties
+  currentDate = new Date();
+  public customers: Customer[] = [];
   public editCustomer: Customer | undefined;
   public deleteCustomer!: Customer;
-  dtoptions:DataTables.Settings={}
-  dtTriger:Subject<any>=new Subject<any>();
-  constructor( private customerService: CustomerService) { }
+  dtoptions: DataTables.Settings = {};
+  dtTriger: Subject<any> = new Subject<any>();
+
+  // Inject customer service
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit(): void {
-    this.dtoptions={
+    // Define DataTables options
+    this.dtoptions = {
       pagingType: 'full_numbers',
       destroy: true,
       columns: [
         // Define your columns here...
         { data: 'name', orderable: true },
-        { data: 'address', orderable: false } ,
+        { data: 'address', orderable: false },
         { data: 'contactNumber', orderable: false },
-        { data: 'nic_number', orderable: true } ,
-        { data: 'customer.address', orderable: true  },
-        { data: 'contactPersonName', orderable: false } ,
+        { data: 'nic_number', orderable: true },
+        { data: 'customer.address', orderable: true },
+        { data: 'contactPersonName', orderable: false },
         { data: 'designation', orderable: false },
         { data: 'email', orderable: false },
         { data: 'customercode', orderable: false },
-         // Disable sorting for this column
+        // Disable sorting for this column
       ],
-      order: []
-      
+      order: [],
     };
+    // Call getCustomers method to retrieve customer data
     this.getCustomers();
   }
 
-  public getCustomers():void { {
-    this.customerService.getCustomerList().subscribe(
-      (response: Customer[]) =>{
-        this.customers = response;
-        this.dtTriger.next(null);
-        console.log(this.customers);
+  // Retrieve customer data using customer service
+  public getCustomers(): void {
+    {
+      this.customerService.getCustomerList().subscribe(
+        (response: Customer[]) => {
+          this.customers = response;
+          this.dtTriger.next(null);
+          console.log(this.customers);
+        },
+        (error: HttpErrorResponse) => alert(error.message)
+      );
+    }
+  }
+
+  // Add new customer using customer service and reset form
+  public onAddCustomer(addForm: NgForm): void {
+    (document.getElementById('add-customer-form') as HTMLInputElement).click();
+    this.customerService.addCustomer(addForm.value).subscribe(
+      (response: Customer) => {
+        console.log(response);
+        this.getCustomers();
+        this.dtoptions = {
+          retrieve: true,
+        };
+
+        addForm.reset();
       },
-      (error: HttpErrorResponse) =>
-         alert(error.message)
-        
-      ); }}
-
-      public onAddCustomer(addForm: NgForm):void{
-         (document.getElementById("add-customer-form") as HTMLInputElement).click();
-        this.customerService.addCustomer(addForm.value).subscribe(
-          (response: Customer)=> {
-            console.log(response);
-            this.getCustomers();
-            this.dtoptions={
-              retrieve: true,
-            };
-           
-            addForm.reset();
-          },
-          (error: HttpErrorResponse) =>  {
-            alert(error.message);
-
-          } 
-        );
+      (error: HttpErrorResponse) => {
+        alert(error.message);
       }
-      public onDeleteCustomer(customerId: number):void{
-        
-       this.customerService.deleteCustomer(customerId).subscribe(
-         (response: void)=> {
-           console.log(response);
-           this.getCustomers();
-           this.dtoptions={
-            retrieve: true,
-          };
-          
-         },
-         (error: HttpErrorResponse) =>  {
-           alert(error.message);
+    );
+  }
 
-         } 
-       );
-     }
+  // Delete customer using customer service
+  public onDeleteCustomer(customerId: number): void {
+    this.customerService.deleteCustomer(customerId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getCustomers();
+        this.dtoptions = {
+          retrieve: true,
+        };
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 
-      public onUpdateCustomer(Customer: Customer):void{
-        
-       this.customerService.updateCustomer(Customer).subscribe(
-         (response: Customer)=> {
-           console.log(response);
-           this.getCustomers();
-           this.dtoptions={
-            retrieve: true,
-          };
-         },
-         (error: HttpErrorResponse) =>  {
-           alert(error.message);
+  // Update customer using customer service
+  public onUpdateCustomer(Customer: Customer): void {
+    this.customerService.updateCustomer(Customer).subscribe(
+      (response: Customer) => {
+        console.log(response);
+        this.getCustomers();
+        this.dtoptions = {
+          retrieve: true,
+        };
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 
-         } 
-       );
-     }
-
-public searchCustomer(key: string): void{
-  console.log(key);
-  const results: Customer[] =[];
-  for (const customer of this.customers){
-    if((customer.name.toLowerCase().indexOf(key.toLowerCase()) !== -1)
-    ||(customer.email.toLowerCase().indexOf(key.toLowerCase()) !== -1)
-    ||(customer.contactPersonName.toLowerCase().indexOf(key.toLowerCase()) !== -1)
-    )
-      {
+  // Search customers by name, email, or contact person name
+  public searchCustomer(key: string): void {
+    console.log(key);
+    const results: Customer[] = [];
+    for (const customer of this.customers) {
+      if (
+        customer.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        customer.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        customer.contactPersonName.toLowerCase().indexOf(key.toLowerCase()) !==
+          -1
+      ) {
         results.push(customer);
       }
+    }
+    this.customers = results;
+    if (results.length === 0 || !key) {
+      this.getCustomers();
+    }
   }
- this.customers =results;
- if(results.length === 0 || !key){
-  this.getCustomers();
- }
-}
 
+  // Function to handle opening modal dialog for editing or deleting a customer
+  public onOpenModal(customer: Customer, mode: string): void {
+    const container = document.getElementById(
+      'main-container'
+    ) as HTMLInputElement;
 
-      public onOpenModal(customer:Customer, mode:string):void {
-        const container = (document.getElementById('main-container') as HTMLInputElement);
-          const button = document.createElement('button');
-          button.type = 'button';
-          button.style.display = 'none';
-          button.setAttribute('data-bs-toggle','modal');
-          if(mode === 'edit'){
-            button.setAttribute('data-bs-target','#exampleModal2');
-            this.editCustomer = customer;
-          }
-          if(mode === 'delete'){
-            button.setAttribute('data-bs-target','#exampleModal3');
-            this.deleteCustomer = customer;
-          }
-          container.appendChild(button);
-          button.click();
-      }
+    // Create a hidden button element
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+
+    // If mode is 'edit', set data-bs-target attribute to edit modal and assign customer to editCustomer property
+    if (mode === 'edit') {
+      button.setAttribute('data-bs-target', '#exampleModal2');
+      this.editCustomer = customer;
+    }
+
+    // If mode is 'delete', set data-bs-target attribute to delete modal and assign customer to deleteCustomer property
+    if (mode === 'delete') {
+      button.setAttribute('data-bs-target', '#exampleModal3');
+      this.deleteCustomer = customer;
+    }
+
+    // Append button to main container element and trigger a click event
+    container.appendChild(button);
+    button.click();
+  }
 }
--- Client/src/app/job/job.service.ts
+++ Client/src/app/job/job.service.ts
@@ -3,25 +3,33 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Job } from './job';
+
 @Injectable({
   providedIn: 'root',
 })
 export class JobService {
+  // Initialize the base URL from the environment variable
   private baseUrl = environment.baseurl;
-  constructor( private httpClient : HttpClient) { }
 
-  public getJobList(): Observable<Job[]>{
+  constructor(private httpClient: HttpClient) { }
+
+  // Retrieves a list of all jobs
+  public getJobList(): Observable<Job[]> {
     return this.httpClient.get<Job[]>(`${this.baseUrl}/job/all`);
   }
 
-   public addJob(job:Job): Observable<Job>{
-    return this.httpClient.post<Job>(`${this.baseUrl}/job/add` ,job);
+  // Adds a new job
+  public addJob(job: Job): Observable<Job> {
+    return this.httpClient.post<Job>(`${this.baseUrl}/job/add`, job);
   }
 
-  public updateJob(job:Job): Observable<Job>{
-    return this.httpClient.put<Job>(`${this.baseUrl}/job/update` ,job);
+  // Updates an existing job
+  public updateJob(job: Job): Observable<Job> {
+    return this.httpClient.put<Job>(`${this.baseUrl}/job/update`, job);
   }
-  public deleteJob(jobId:Number): Observable<void>{
-    return this.httpClient.delete<void>(`${this.baseUrl}/job/delete/${jobId}` );
+
+  // Deletes an existing job
+  public deleteJob(jobId: Number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.baseUrl}/job/delete/${jobId}`);
   }
 }
