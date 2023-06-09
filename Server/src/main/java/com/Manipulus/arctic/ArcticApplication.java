71fe67892a3c801aae4c1d3b50ef2d801c183ff6
MaliVitha <86399536+MaliVitha@users.noreply.github.com>
2023-06-09 19:53:19 +0530

    MAN 431- Generated PDF
    Generated PDF

--- Server/src/main/java/com/Manipulus/arctic/ArcticApplication.java
+++ Server/src/main/java/com/Manipulus/arctic/ArcticApplication.java
@@ -1,5 +1,6 @@
 package com.Manipulus.arctic;
 
+import net.sf.jasperreports.engine.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +9,25 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ArcticApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JRException {
+
 		SpringApplication.run(ArcticApplication.class, args);
+		String filePath="C:\\Manipulus 7.0\\Manipulus\\Server\\src\\main\\resources\\template\\gatePass.jrxml";
+		Map<String,Object> parameters=new HashMap<>();
+		parameters.put("siteVisitId",123);
+		parameters.put("jobId",456);
+		parameters.put("scheduledDate",java.sql.Date.valueOf("2022-07-07"));
+		parameters.put("endDate",java.sql.Date.valueOf("2022-07-08"));
+		JasperReport report= JasperCompileManager.compileReport(filePath);
+		JasperPrint print= JasperFillManager.fillReport(report,parameters,new JREmptyDataSource());
+		JasperExportManager.exportReportToPdfFile(print,"C:\\Manipulus 7.0\\Manipulus\\Server\\src\\main\\resources\\static\\gatePass.pdf");
+		System.out.println("Report Generated Successfully");
 	}
 	@Bean
 	public CorsFilter corsFilter() {
