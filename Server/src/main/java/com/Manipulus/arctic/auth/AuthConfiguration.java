a413ac33bbfe7e93f65def3070d8331b45e0b98b
sampath-kumaramd <pmmdskk@gmail.com>
2023-06-12 01:49:22 +0530

    update auth

--- Server/src/main/java/com/Manipulus/arctic/auth/AuthConfiguration.java
+++ Server/src/main/java/com/Manipulus/arctic/auth/AuthConfiguration.java
@@ -36,6 +36,8 @@ public class AuthConfiguration {
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests().requestMatchers("/refresh-token/**").permitAll();
+        http.authorizeHttpRequests().requestMatchers("/vehicle/{id}/photo**").permitAll();
+        http.authorizeHttpRequests().requestMatchers("/vehicle/**").permitAll();
         http.authorizeHttpRequests().requestMatchers("/forgot-password/**").permitAll();
         http.authorizeHttpRequests().anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)),jwtHelper));
