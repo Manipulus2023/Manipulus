96e380928f171b0a704036257c38b6d3d9c051c7
sampath-kumaramd <pmmdskk@gmail.com>
2023-06-13 19:53:52 +0530

    Merge branch 'Development-merge' into MAN-25-Back-end

--- Server/src/main/java/com/Manipulus/arctic/Agreement/entity/Agreement.java
+++ Server/src/main/java/com/Manipulus/arctic/Agreement/entity/Agreement.java
@@ -25,7 +25,7 @@ public class Agreement {
     @Column(name="expired_date",length =10)
     private String expired_date;
 
-    @Column(name="customerId",length = 30)
+    @Column(name="customerId",length = 10)
     private String NIC;
 
 
